18144646082ac07a6350ba17af60d560bc70b621
bericktk <br.ericktk@gmail.com>
2025-03-10 22:13:30 -0300

    Adiciona tratamento de exceções e finalização do Scanner na classe Main

--- exercicios/exercicios_java/Main.java
+++ exercicios/exercicios_java/Main.java
@@ -5,25 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        TaskList taskList = new TaskList();
+        Scanner sc = new Scanner(System.in);
+        try {
+            TaskList taskList = new TaskList();
+            while (true){
+                System.out.println("Insira a opção desejada");
+                String opcao = sc.nextLine();
 
-        while (true){
-            System.out.println("Insira a opção desejada");
-            Scanner sc = new Scanner(System.in);
-            String opcao = sc.nextLine();
+                if(opcao .equals("1")){
+                    System.out.println("Insira a tarefa");
+                    String tasks = sc.nextLine();
+                    taskList.addTask(tasks);
+                    System.out.println("Tarefa Adicionada com Sucesso");
 
-            if(opcao .equals("1")){
-                System.out.println("Insira a tarefa");
-                String tasks = sc.nextLine();
-                taskList.addTask(tasks);
-                System.out.println("Tarefa Adicionada com Sucesso");
-
-            }else if(opcao .equals("2")){
-                taskList.listTasks();
-            }else{
-                break;
+                }else if(opcao .equals("2")){
+                    taskList.listTasks();
+                }else{
+                    break;
+                }
             }
-
+        } catch (Exception e) {
+            System.out.println("Erro ao executar o programa");
+            e.printStackTrace();
+        }finally{
+            System.out.println("Programa finalizado");
+            sc.close();
         }
+        
     }
 }
