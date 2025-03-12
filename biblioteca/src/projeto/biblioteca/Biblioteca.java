2479a012fe16bc802e6d8ced19dedfd2bd1eda7c
bericktk <br.ericktk@gmail.com>
2025-03-11 23:39:11 -0300

    Adiciona classes Biblioteca e Livro para gerenciamento de acervo e operações de empréstimo

---
+++ biblioteca/src/projeto/biblioteca/Biblioteca.java
@@ -0,0 +1,71 @@
+package projeto.biblioteca;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+
+    ArrayList<Livro> livros = new ArrayList<Livro>();
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+        System.out.println("Livro adicionado com sucesso!.");
+    }
+
+    public void removerLivro(Livro livro) {
+        livros.remove(livro);
+        System.out.println("Livro removido com sucesso!.");
+    }
+
+    public void buscarLivro(int ISBN) {
+        for (Livro livro : livros) {
+            if (livro.getISBN() == ISBN) {
+                System.out.println("Livro encontrado: " + livro.getTitulo());
+                return;
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+    public void listarLivros() {
+        for (Livro livro : livros) {
+            System.out.println("Título: " + livro.getTitulo());
+            System.out.println("Autor: " + livro.getAutor());
+            System.out.println("ISBN: " + livro.getISBN());
+            System.out.println("Ano de publicação: " + livro.getAno_publicacao());
+            System.out.println("Emprestado: " + livro.isEmprestado());
+            System.out.println();
+        }
+    }
+
+    public void emprestarLivro(int ISBN) {
+        for (Livro livro : livros) {
+            if (livro.getISBN() == ISBN) {
+                if (livro.isEmprestado() == false) {
+                    livro.setEmprestado(true);
+                    System.out.println("Livro emprestado.");
+                    return;
+                } else {
+                    System.out.println("Livro já emprestado.");
+                    return;
+                }
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+    public void devolverLivro(int ISBN) {
+        for (Livro livro : livros) {
+            if (livro.getISBN() == ISBN) {
+                if (livro.isEmprestado() == true) {
+                    livro.setEmprestado(false);
+                    System.out.println("Livro devolvido.");
+                    return;
+                } else {
+                    System.out.println("Livro não emprestado.");
+                    return;
+                }
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+}
---
+++ biblioteca/src/projeto/biblioteca/Livro.java
@@ -0,0 +1,77 @@
+package projeto.biblioteca;
+
+public class Livro {
+    
+    private String titulo;
+    private String autor;
+    private int ISBN;
+    private int ano_publicacao;
+    private boolean emprestado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public int getAno_publicacao() {
+        return ano_publicacao;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setAno_publicacao(int ano_publicacao) {
+        this.ano_publicacao = ano_publicacao;
+    }
+
+    public void setEmprestado(boolean emprestado) {
+        this.emprestado = emprestado;
+    }
+
+    public Livro(String titulo, String autor, int ISBN, int ano_publicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.ano_publicacao = ano_publicacao;
+        this.emprestado = false;
+    }
+
+    void emprestado() {
+        if (this.emprestado == false) {
+            this.emprestado = true;
+        } else {
+            System.out.println("Livro já emprestado.");
+        }
+    }
+
+    public void devolvido() {
+        if (this.emprestado == true) {
+            this.emprestado = false;
+            System.out.println("Livro devolvido a biblioteca.");
+        } else {
+            System.out.println("Livro já está na biblioteca.");
+        }
+    }
+
+
+}
